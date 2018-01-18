1e9a0dbda05e88e33f1c2f6f9f27240169e7dad9
Miguel Ternero <migueltern@gmail.com>
2018-01-18 16:21:28 +0100

    traduccion

--- src/main/java/es/egc/tasks/Application.java
+++ src/main/java/es/egc/tasks/Application.java
@@ -12,7 +12,7 @@ public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-		logger.info("Initializing application...");
+		logger.info("Inicializando aplicacion...");
         SpringApplication.run(Application.class, args);
     }
 
